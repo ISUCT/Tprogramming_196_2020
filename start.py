e5301690da7419b304881542f69fbd9bc2549415
ksenia <ksenialadova451@gmail.com>
2020-04-09 19:55:15 +0300

    Fixed review points

--- start.py
+++ start.py
@@ -1,23 +1,22 @@
-print("Hello world")
 import math
 
-def calc(a,b,x):
-    y = ((a + b*x)**(2.5))/(1 + math.log10(a + b*x)) # 18
+def calc(x):
+    y = (math.fabs(x**2 - 2.5))**(1/4) + (math.log10(x**2))**(1/3)
     return y
 
-def task_a(a,b,xn,xk,dx):
+def task_a(xn,xk,dx):
     x = xn
     res = []
     while x<=xk:
-        y = calc(a,b,x)
+        y = calc(x)
         res.append((x, y))
         x += dx
     return res
 
-def task_b(a,b,x):
+def task_b(x):
     res = []
     for x_item in x:
-        y = calc(a,b,x_item)
+        y = calc(x_item)
         res.append((x_item, y))
     return res
 
@@ -28,14 +27,12 @@ def print_res(res):
 
 
 if __name__ == "__main__":
-    a = 2.5
-    b = 4.6
-    res = task_a(a,b,1.1,3.6,0.5)
+    res = task_a(1.1,3.6,0.5)
     print("Task A ------------")
     print_res(res)
 
     x_lst = [1.2, 1.28, 1.36, 1.46, 2.35]
-    res = task_b(a,b,x_lst)
+    res = task_b(x_lst)
 
     print("Task B ------------")
     print_res(res)
